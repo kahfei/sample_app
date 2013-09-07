014288eb8142697921dcf341314bb51d83944b1a
kahfei <bak.kahfei@gmail.com>
2013-09-07 18:36:29 +0800

    completing chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,6 +25,28 @@ describe "User Pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+    end
+
+#added, to test submission without email
+    describe "with missing email" do
+      before do 
+        fill_in "Name", with: "Example User 2"
+        fill_in "Email", with: ""
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+        it { should have_selector('div.alert.alert-error') }
+        it { should have_content('Email') }
+      end
     end
 
     describe "with valid information" do
@@ -37,6 +59,14 @@ describe "User Pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome')}
       end
     end
   end
